312d2ff33e354834eb92c3a269217a4c49354892
nityanandagohain <nityanandagohain@gmail.com>
2024-10-30 21:47:01 +0530

    fix: update dependencies and config

--- src/main/java/com/example/log4j_demo/HelloController.java
+++ src/main/java/com/example/log4j_demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.log4j_demo;
 
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,13 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
+        try {
+        ThreadContext.put("userId", "myuserId");
+        ThreadContext.put("requestId", "sdf");
         logger.info("Hello endpoint was called");
         return "Hello, World!";
+        } finally {
+            ThreadContext.clearAll();
+        }
     }
 }
